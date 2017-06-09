6ac911f363e95de4e57f7fec9fdb52edb79ec8a9
Zishrodrigues <zish.rodrigues@gmail.com>
2017-06-09 20:34:37 +0200

    added styling and api call

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 var express = require('express');
 var app = express();
 var request = require('request');
+require('dotenv').config();
 var http = require('http').Server(app);
+
+var url = process.env.APIURL;
 
 app.use(express.static('static'));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.get('/', function (req, res) {
-    res.render('pages/index');
+    // res.render('pages/index');
+    console.log(url);
+    load(url, callback);
+    function callback(data) {
+      res.render('pages/index', { articles: data });
+      console.log(data);
+  }
 });
+
+function load(url, callback) {
+  request(url, function(err, res, body) {
+    if (err) console.warn(err);
+    callback(JSON.parse(body));
+  });
+}
 
 http.listen(1337, function(){
   console.log('listening on 1337');
