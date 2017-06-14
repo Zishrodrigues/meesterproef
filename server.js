c8e66bf05603de10242553734b9a9d46d2535c82
Zishrodrigues <zish.rodrigues@gmail.com>
2017-06-14 18:14:00 +0200

    added sockets.io and comments base

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var request = require('request');
 require('dotenv').config();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 var url = process.env.APIURL;
 
@@ -39,6 +40,18 @@ function load(url, callback) {
     callback(JSON.parse(body));
   });
 }
+
+io.on('connection', function(socket){
+    console.log('Hey there!');
+
+    socket.on('place comment', function(comment){
+        io.emit('place comment', comment);
+    });
+
+    socket.on('disconnect', function(){
+        console.log('Bi bi : <');
+    });
+});
 
 http.listen(1337, function(){
   console.log('listening on 1337');
--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,15 @@
 (function() {
     "use strict";
 
+    var socket = io();
+
     var config = {
         elements: {
             todayHeader: document.getElementById('todayHeader'),
-            dayOne: document.getElementById('dayOne')
+            dayOne: document.getElementById('dayOne'),
+            commentsList: document.getElementById('comments'),
+            commentForm: document.getElementById('commentForm'),
+            commentInput: document.getElementById('commentInput')
         }
     };
 
@@ -12,6 +17,7 @@
         init: function() {
             console.log('app started :)');
             listDates.setDay();
+            comments.placeComment();
         }
     };
 
@@ -20,8 +26,25 @@
         setDay: function() {
             var date = new Date();
             var dayName = listDates.dayNames[date.getDay()];
-            config.elements.todayHeader.innerHTML = dayName;
-            config.elements.dayOne.innerHTML = listDates.dayNames[date.getDay() - 1];   
+            if(window.location.pathname == '/') {
+                config.elements.todayHeader.innerHTML = dayName;
+                config.elements.dayOne.innerHTML = listDates.dayNames[date.getDay() - 1];
+            }
+        }
+    };
+
+    var comments = {
+        placeComment: function(){
+            config.elements.commentForm.onsubmit = function() {
+                var value = commentInput.value;
+                socket.emit('place comment', value);
+                value = '';
+                return false;
+            };
+            socket.on('place comment', function(msg) {
+                var listItem = document.createElement('li');
+                config.elements.commentsList.appendChild(listItem).innerHTML=(msg);
+            });
         }
     };
 
